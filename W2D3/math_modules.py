90681b383685ef94366b34ff0d293948df85ab20
linkqueen <102402701+linkqueen@users.noreply.github.com>
2022-04-14 15:28:20 -0700

    Add files via upload

---
+++ W2D3/math_modules.py
@@ -0,0 +1,12 @@
+def square_footage(length,width):
+    area = length * width
+    return area
+
+    
+def circumference(radius):
+    from math import pi
+    circle = 2*pi*radius
+    return circle
+
+    
+    
